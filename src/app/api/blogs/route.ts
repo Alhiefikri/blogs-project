8adcf98ed3eb3baa0993add0ec7bd501cb45fe49
alhiefikri <alhiepedia@gmail.com>
2025-02-28 01:08:17 +0800

    feat: route handling method GET

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -4,8 +4,35 @@ import { NextRequest, NextResponse } from "next/server";
 import { Blog } from "@/models/blog";
 
 export async function GET(req: NextRequest) {
-  await connectDB(mongo_uri);
-  return new NextResponse("GET BLOGS");
+  const searchParams = req.nextUrl.searchParams;
+  const limit = searchParams.get("limit") || "10";
+  const page = searchParams.get("page") || "1";
+  const skip = (parseInt(page) - 1) * parseInt(limit);
+
+  try {
+    await connectDB(mongo_uri);
+    const blogs = await Blog.find()
+      .sort({ date: -1 })
+      .skip(skip)
+      .limit(parseInt(limit));
+    const totalData = await Blog.countDocuments();
+    return NextResponse.json({
+      total: totalData,
+      page: page,
+      limit: limit,
+      data: blogs,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
 export async function POST(req: NextRequest) {
   try {
