ecabda78e9aef5b50f159cac21b356748d75bda2
alhiefikri <alhiepedia@gmail.com>
2025-02-28 00:40:08 +0800

    feat: setup schema and type models

---
+++ src/models/blog.ts
@@ -0,0 +1,22 @@
+import { Blogs } from "@/types";
+import { model, models, Schema } from "mongoose";
+
+const blogSchema = new Schema<Blogs>(
+  {
+    title: {
+      type: String,
+      required: [true, "Title should not be empty"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description should not be empty"],
+    },
+    imageURL: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Blog = models.Blog || model<Blogs>("Blogs", blogSchema);
