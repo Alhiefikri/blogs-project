bacd0fb754b9659c680f0d8ddf7b1a809d5e6362
alhiefikri <alhiepedia@gmail.com>
2025-02-28 09:34:27 +0800

    update: mongo server

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
 export const mongo_uri =
-  "mongodb+srv://alhiefikri:alhiefikri@alhie-blog.kednz.mongodb.net/?retryWrites=true&w=majority&appName=alhie-blog";
+  "mongodb://alhiefikri:alhiefikri@alhie-blog-shard-00-00.kednz.mongodb.net:27017,alhie-blog-shard-00-01.kednz.mongodb.net:27017,alhie-blog-shard-00-02.kednz.mongodb.net:27017/?ssl=true&replicaSet=atlas-ddrux7-shard-0&authSource=admin&retryWrites=true&w=majority&appName=alhie-blog";
