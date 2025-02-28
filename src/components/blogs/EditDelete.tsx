985d4375886cbe2d964294f214c008bf1db8b4e8
alhiefikri <alhiepedia@gmail.com>
2025-03-01 00:01:59 +0800

    feat: blogDetail section

---
+++ src/components/blogs/EditDelete.tsx
@@ -0,0 +1,39 @@
+import { Delete, Edit } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function EditDelete() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost">...</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Edit />
+            <span>Edit</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Delete />
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
